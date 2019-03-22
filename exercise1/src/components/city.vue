0765e5665b31abb3f4840052b8250f5aa8713056
gitwho <2387639696@qq.com>
2019-03-22 18:03:26 +0800

    add

---
+++ exercise1/src/components/city.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="city">
+    <div class="nav-tit">
+      <i></i>
+      <span class="title">城市选择</span>
+      <div>
+        <input type="text" placeholder="输入">
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ exercise1/src/components/home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="nav">
+      <input class="search" type="text" placeholder="输入城市">
+      <div class="location" @click="chooseCity">选择城市</div>
+    </div>
+    <swiper-nav></swiper-nav>
+  </div>
+</template>
+
+<script>
+import Location from '@/base/location'
+import SwiperNav from '@/base/swiper-nav'
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    chooseCity() {
+      this.$router.push('/city')
+    }
+  },
+  components: {
+    Location,
+    SwiperNav
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.nav
+  height 40px
+  width 100%
+  background-color #fded32
+  display flex
+  align-items center
+
+  .search
+    height 32px
+    line-height 32px
+    color #dddddd
+    border-radius 5px
+    margin-left 20px
+    padding-left 10px
+    width 250px
+    font-family "微软雅黑"
+    font-size 16px
+  .location
+    height 32px
+    line-height 32px
+    font-size 16px
+
+</style>
