ddfe8aebad658831e7e53e5ffb7007d9f6a50aa9
gitwho <2387639696@qq.com>
2019-05-01 16:16:38 +0800

    add

---
+++ WeChat/chat-react/src/views/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Nav from './Nav'
+import Chat from './Chat'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        {/* 左边 */}
+        <Nav></Nav>
+        {/* 右边 */}
+        <Chat></Chat>
+      </div>
+    )
+  }
+}
